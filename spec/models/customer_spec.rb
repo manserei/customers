f7dae03a2b41dc6588773197afe33553044fafc0
Rich Barton <richard.barton@xing.com>
2012-12-13 15:53:27 +0100

    Use FactoryGirl for model creation

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -12,36 +12,113 @@ require 'spec_helper'
 
 
 describe Customer do
-  before do
-    @customer = Customer.new(
-      :first_name => "Hans Hendrik",
-      :last_name  => "Mans",
-      :email      => "hans@mans.de")
-  end
+  subject { FactoryGirl.build(:customer) }
 
   describe '#full_name' do
     it "should return the full name" do
-      @customer.full_name.should == "Mans, Hans Hendrik"
+      subject.full_name.should == "#{subject.last_name}, #{subject.first_name}"
     end
   end
 
   describe '#to_param' do
     before do
-      @customer.save
+      subject.save!
     end
 
     it "should return a sluggified version of the first name" do
-      @customer.to_param.should == "#{@customer.id}-hans-hendrik"
+      subject.to_param.should == "#{subject.id}-#{subject.first_name.parameterize}"
     end
   end
 
-  context "with the last_name missing" do
+  context "validating" do
+
+    context "with an unfeasibly short first name" do
+      before do
+        subject.first_name = "Mo"
+      end
+
+      it "should not be valid" do
+        subject.should_not be_valid
+      end
+    end
+
+    context "with the last_name missing" do
+      before do
+        subject.last_name = ""
+      end
+
+      it "should not be valid" do
+        subject.should_not be_valid
+      end
+    end
+
+
+    context "email" do
+      context "with an invalid email" do
+        before do
+          subject.email = "fake_email"
+        end
+
+        it "should not be valid" do
+          subject.should_not be_valid
+        end
+      end
+
+      context "defunct email domains" do
+
+        before do
+          subject.email = "reallynoinfo@aol.com"
+        end
+
+        it "should not be valid" do
+          subject.should_not be_valid
+        end
+
+      end
+
+      context "unique email address" do
+
+        before do
+          customer = FactoryGirl.create(:customer)
+
+          # Customer.create(
+          #   :first_name => "Jeff",
+          #   :last_name  => "Winters",
+          #   :email      => "jeff@winters.com")
+          subject.email = customer.email
+        end
+
+        it "should not be valid" do
+          subject.should_not be_valid
+        end
+
+      end
+
+    end
+
+    context "maximum projects" do
+      before do
+        subject.save!
+        10.times do |i|
+          subject.projects.create({ :title => "Project #{i}" })
+        end
+        subject.projects.new({ :title => "Project 11" })
+      end
+
+      it "shouldn't add more than the maximum" do
+          subject.should_not be_valid
+      end
+
+    end
+  end
+
+  describe "destroy" do
     before do
-      @customer.last_name = ""
+      subject.save!
     end
 
-    it "should not be valid" do
-      @customer.should_not be_valid
-    end
+    it { expect { subject.destroy }.to change(Customer, :count).by(-1) }
   end
+
+
 end
