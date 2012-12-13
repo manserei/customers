1260adbbeff2776233e5b052334dbb1103a23f9f
Hendrik Mans <hendrik@mans.de>
2012-12-13 14:59:05 +0100

    Added :customer_from_germany factory

--- spec/factories/customers.rb
+++ spec/factories/customers.rb
@@ -5,5 +5,10 @@ FactoryGirl.define do
     birthday   { (10000 + rand(5000)).days.ago }
 
     email      { Faker::Internet.email }
+
+    factory :customer_from_germany do
+      first_name { Faker::NameDE.first_name }
+      last_name  { Faker::NameDE.last_name }
+    end
   end
 end
