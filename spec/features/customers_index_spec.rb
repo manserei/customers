085741f180f7e0849457bbbf0db5426706177968
Hendrik Mans <hendrik@mans.de>
2012-12-14 13:03:24 +0100

    Flesh out customer spec and use Selenium

--- spec/features/customers_index_spec.rb
+++ spec/features/customers_index_spec.rb
@@ -6,7 +6,11 @@ describe "the customers index" do
     visit "/customers"
   end
 
-  it "displays a list of all customers" do
+  it "displays a list of all customers", js: true do
+    Customer.count.should == 10
+
+    page.should have_selector 'h1', text: 'List of all customers'
+
     Customer.all.each do |customer|
       page.should have_selector "td", text: customer.full_name
     end
